ef6c19e21c9acd6452dfab27a385598c1d690b0e
Jesse McKinney <jessman51386@gmail.com>
2020-04-28 15:13:36 -0400

    yay

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -9,7 +9,7 @@ const NavBar = props => {
         <AppBar position="static" {...props}>
             <Toolbar>
                 <Typography variant="headline" color="inherit" align="center">
-                The ArteMeso Clicky Game
+                The Awesome Clicky Game
                 </Typography>
             </Toolbar>
         </AppBar>
